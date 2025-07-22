eed0b3e63a3aabf81da3be99c944beddb335ee01
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-07-22 20:27:50 -0300

    feat(extension): add extensions file to configure dependency injection

---
+++ HealthCheckApi/Extensions/Extensions.cs
@@ -0,0 +1,21 @@
+using FluentValidation;
+using HealthCheckApi.Dto;
+using HealthCheckApi.Repository;
+using HealthCheckApi.Repository.Abstractions;
+using HealthCheckApi.Services;
+using HealthCheckApi.Services.Abstractions;
+using HealthCheckApi.Validators;
+
+namespace HealthCheckApi.Extensions;
+
+public static class Extensions
+{
+    public static IServiceCollection AddDependencies(this IServiceCollection services)
+    {
+        services.AddTransient<IUserRepository, UserRepository>();
+        services.AddScoped<IValidator<CreateUserRequest>, CreateUserRequestValidator>();
+        services.AddScoped<IUserService, UserService>();
+
+        return services;
+    }
+}
