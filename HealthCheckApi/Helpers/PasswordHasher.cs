36bf734e81153c5e12c0fb9501c988c3d31fb0e9
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-07-28 20:04:04 -0300

    feat(helper): add password hasher to encrypt passwords and compare passwords

--- HealthCheckApi/Helpers/PasswordHasher.cs
+++ HealthCheckApi/Helpers/PasswordHasher.cs
@@ -3,7 +3,13 @@ using BCrypt.Net;
 
 public static class PasswordHasher
 {
-    public static string HashPassword(string password) {
+    public static string HashPassword(string password)
+    {
         return BCrypt.HashPassword(password, BCrypt.GenerateSalt(12));
+    }
+
+    public static bool VerifyPassword(string passwordHashed, string password)
+    {
+        return BCrypt.Verify(password, passwordHashed);
     }
 }
