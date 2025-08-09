9726ddd5cfedc7fdf1029a5c61671faa427f3f1b
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-08-08 21:08:34 -0300

    refactor(config): set database provider

--- HealthCheckApi/Program.cs
+++ HealthCheckApi/Program.cs
@@ -1,13 +1,16 @@
 using System.Text.Json.Serialization;
 using HealthCheckApi.Data;
 using HealthCheckApi.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
 builder.Services.AddDependencies(builder.Configuration);
 builder.Services.AddControllers()
 .AddJsonOptions(options =>
